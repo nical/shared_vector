7428e08247d3ba61627fc1c5090bc2ab4a654fcd
Nicolas Silva <nical@fastmail.com>
2023-02-19 17:28:48 +0100

    Add basic fuzzing for SharedVector and UniqueVector.

---
+++ fuzz/fuzz_targets/cmd.rs
@@ -0,0 +1,113 @@
+use arbitrary::Arbitrary;
+
+pub fn slot(idx: usize) -> usize { idx % 4 }
+
+#[derive(Arbitrary, Copy, Clone, Debug)]
+pub enum Cmd {
+    AddRef { src_idx: usize, dst_idx: usize },
+    DropVec { idx: usize },
+    Clear { idx: usize },
+    Push { idx: usize, val: u32 },
+    PushSlice { idx: usize },
+    Pop { idx: usize },
+    CloneBuffer { src_idx: usize, dst_idx: usize },
+    Iter { idx: usize },
+    IterMut { idx: usize },
+    EnsureUnique { idx: usize },
+    PushVector { src_idx: usize, dst_idx: usize },
+    WithCapacity { idx: usize, cap: usize },
+    FromSlice { src_idx: usize, dst_idx: usize },
+    AsMutSlice { idx: usize },
+    First { idx: usize },
+    Last { idx: usize, },
+    FirstMut { idx: usize },
+    LastMut { idx: usize, },
+    Reserve { idx: usize, val: usize },
+}
+
+fn cmd_to_string(vec_type: &str, cmd: Cmd) -> String {
+    match cmd {
+        Cmd::AddRef { src_idx, dst_idx } => {
+            if vec_type == "UniqueVector" {
+                return String::new();
+            }
+            let src_idx = slot(src_idx);
+            let dst_idx = slot(dst_idx);
+            format!("vectors[{dst_idx}] = vectors[{src_idx}].new_ref();")
+        }
+        Cmd::DropVec { idx } => {
+            format!("vectors[{}] = {vec_type}::new();", slot(idx))
+        }
+        Cmd::Clear { idx } => {
+            format!("vectors[{}].clear();", slot(idx))
+        }
+        Cmd::Push { idx, val } => {
+            format!("vectors[{}].push(Box::new({val}));", slot(idx))
+        }
+        Cmd::PushSlice { idx } => {
+            format!("vectors[{}].push_slice(&[Box::new(1), Box::new(2), Box::new(3)]);", slot(idx)) 
+        }
+        Cmd::Pop { idx } => {
+            format!("vectors[{}].pop();", slot(idx))
+        }
+        Cmd::CloneBuffer { src_idx, dst_idx } => {
+            let src_idx = slot(src_idx);
+            let dst_idx = slot(dst_idx);
+            format!("vectors[{dst_idx}] = vectors[{src_idx}].clone_buffer();")
+        }
+        Cmd::Iter { idx } => {
+            format!("let _: u32 = vectors[{}].iter().fold(0, |a, b| a.wrapping_add(**b));", slot(idx))
+        }
+        Cmd::IterMut { idx } => {
+            format!("for elt in vectors[{}].iter_mut() {{ *elt = Box::new(1337); }}", slot(idx))
+        }
+        Cmd::EnsureUnique { idx } => {
+            if vec_type == "UniqueVector" {
+                return String::new();
+            }
+            format!("vectors[{}].ensure_unique();", slot(idx))
+        }
+        Cmd::PushVector { src_idx, dst_idx } => {
+            format!("vectors[{}].push_vector(std::mem::replace(&mut vectors[{}], {vec_type}::new()));", slot(dst_idx), slot(src_idx))
+        }
+        Cmd::WithCapacity { idx, cap } => {
+            format!("vectors[{}] = {vec_type}::with_capacity({});", slot(idx), cap % 1024)
+        }
+        Cmd::FromSlice { src_idx, dst_idx } => {
+            format!("vectors[{}] = {vec_type}::from_slice(vectors[{}].as_slice());", slot(dst_idx), slot(src_idx))
+        }
+        Cmd::AsMutSlice { idx } => {
+            format!("for v in vectors[{}].as_mut_slice() {{ *v = Box::new(42); }};", slot(idx))
+        }
+        Cmd::First { idx } => {
+            format!("let _ = vectors[{}].first();", slot(idx))
+        }
+        Cmd::Last { idx } => {
+            format!("let _ = vectors[{}].last();", slot(idx))
+        }
+        Cmd::FirstMut { idx } => {
+            format!("if let Some(elt) = vectors[{}].first_mut() {{ *elt = Box::new(1); }}", slot(idx))
+        }
+        Cmd::LastMut { idx } => {
+            format!("if let Some(elt) = vectors[{}].last_mut() {{ *elt = Box::new(2); }} ", slot(idx))
+        }
+        Cmd::Reserve { idx, val } => {
+            format!("vectors[{}].reserve(({} % 1024).min(2048 - vectors[{}].len()));", slot(idx), val, slot(idx))
+        }
+    }
+}
+
+#[allow(unused)]
+pub fn cmds_to_src(vec_type: &str, cmds: &[Cmd]) {
+    println!("// -------");
+    println!("let mut vectors: [{vec_type}<Box<u32>>; 4] = [");
+    println!("  {vec_type}::new(),");
+    println!("  {vec_type}::new(),");
+    println!("  {vec_type}::new(),");
+    println!("  {vec_type}::new(),");
+    println!("];");
+    for cmd in cmds {
+        println!("{}", cmd_to_string(vec_type, *cmd));
+    }
+    println!("// -------");
+}
---
+++ fuzz/fuzz_targets/unique_vector.rs
@@ -0,0 +1,81 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use shared_vector::UniqueVector;
+
+mod cmd;
+use cmd::*;
+
+fuzz_target!(|cmds: Vec<Cmd>| {
+
+    let mut vectors: [UniqueVector<Box<u32>>; 4] = [
+        UniqueVector::new(),
+        UniqueVector::new(),
+        UniqueVector::new(),
+        UniqueVector::new(),
+    ];
+
+    for cmd in cmds {
+        match cmd {
+            Cmd::AddRef { .. } => {
+                //vectors[slot(dst_idx)] = vectors[slot(src_idx)].new_ref();
+            }
+            Cmd::DropVec { idx } => {
+                vectors[slot(idx)] = UniqueVector::new();
+            }
+            Cmd::Clear { idx } => {
+                vectors[slot(idx)].clear();
+            }
+            Cmd::Push { idx, val } => {
+                vectors[slot(idx)].push(Box::new(val));
+            }
+            Cmd::Pop { idx } => {
+                vectors[slot(idx)].pop();
+            }
+            Cmd::PushSlice { idx } => {
+                vectors[slot(idx)].push_slice(&[Box::new(1), Box::new(2), Box::new(3)]);
+            }
+            Cmd::CloneBuffer { src_idx, dst_idx } => {
+                vectors[slot(dst_idx)] = vectors[slot(src_idx)].clone_buffer();
+                }
+            Cmd::Iter { idx } => {
+                let _: u32 = vectors[slot(idx)]
+                    .iter()
+                    .fold(0, |a, b| a.wrapping_add(**b));
+            }
+            Cmd::IterMut { idx } => {
+                for elt in vectors[slot(idx)].iter_mut() { *elt = Box::new(1337); };
+            }
+            Cmd::EnsureUnique { .. } => {}
+            Cmd::PushVector { src_idx, dst_idx } => {
+                let v = std::mem::replace(&mut vectors[slot(src_idx)], UniqueVector::new());
+                vectors[slot(dst_idx)].push_vector(v);
+            }
+            Cmd::WithCapacity { idx, cap } => {
+                vectors[slot(idx)] = UniqueVector::with_capacity(cap % 1024);
+            }
+            Cmd::FromSlice { src_idx, dst_idx } => {
+                vectors[slot(dst_idx)] = UniqueVector::from_slice(vectors[slot(src_idx)].as_slice());
+            }
+            Cmd::AsMutSlice { idx } => {
+                for v in vectors[slot(idx)].as_mut_slice() { *v = Box::new(42); };
+            }
+            Cmd::First { idx } => {
+                let _ = vectors[slot(idx)].first();
+            }
+            Cmd::Last { idx } => {
+                let _ = vectors[slot(idx)].last();
+            }
+            Cmd::FirstMut { idx } => {
+                if let Some(elt) = vectors[slot(idx)].first_mut() { *elt = Box::new(1); }
+            }
+            Cmd::LastMut { idx } => {
+                if let Some(elt) = vectors[slot(idx)].last_mut() { *elt = Box::new(2); }
+            }
+            Cmd::Reserve { idx, val } => {
+                let val = (val % 1024).min(2048 - vectors[slot(idx)].len());
+                vectors[slot(idx)].reserve(val);
+            }
+        }
+    }
+});
